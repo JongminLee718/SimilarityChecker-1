a5ad68618c43d61533bc08b046e3fa15d5ca2409
jongmin.lee <znargon@gmail.com>
2025-07-02 15:52:49 +0900

    1st - calculate length score

--- similarity_checker.cpp
+++ similarity_checker.cpp
@@ -5,7 +5,25 @@ using namespace std;
 
 class SimilarityChecker {
 public:
+	int checkLength(const string &str1, const string &str2)
+	{
+		int result = 0;
+		int lengthStr1 = str1.size();
+		int lengthStr2 = str2.size();
+		int gap, div;
 
+		if (lengthStr1 > lengthStr2) {
+			gap = (lengthStr1 - lengthStr2)*100;
+			div = lengthStr2;
+		}
+		else {
+			gap = (lengthStr2 - lengthStr1)*100;
+			div = lengthStr1;
+		}
+		result = (100 - gap / div) * 60 / 100;
+
+		return result;
+	}
 
 private:
 
--- similarity_checker_test.cpp
+++ similarity_checker_test.cpp
@@ -9,9 +9,30 @@ public:
 
 };
 
-TEST(SimilarityChecker, tc1) {
-	EXPECT_EQ(1, 1);
+TEST(SimilarityChecker, Length) {
+	SimilarityChecker similarity;
+	int result = 0;
+	result = similarity.checkLength("ABCDE", "ABCDE");
+
+	EXPECT_EQ(60, result);
 }
+
+TEST(SimilarityChecker, Length1) {
+	SimilarityChecker similarity;
+	int result = 0;
+	result = similarity.checkLength("ABC", "ABCDE");
+
+	EXPECT_EQ(20, result);
+}
+
+TEST(SimilarityChecker, Length2) {
+	SimilarityChecker similarity;
+	int result = 0;
+	result = similarity.checkLength("ABCDE", "ABC");
+
+	EXPECT_EQ(20, result);
+}
+
 
 int main() {
 	testing::InitGoogleMock();
